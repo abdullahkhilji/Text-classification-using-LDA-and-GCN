564441b24e90305aedb099d2c7fb3ed134c9e663
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-10-05 23:32:51 +0530

    Final addition, before changing workstation.

--- step_1_data_to_pandas_normal (copy).py
+++ step_1_data_to_pandas_normal (copy).py
@@ -15,24 +15,24 @@ if ".DS_Store" in labels:
 all_data = []
 
 # uncomment to debug
-# labels = labels[:5]
+labels = labels[:5]
 counter = 0
 
 for i in labels:
     instances_in_a_label = os.listdir ("training_dataset/" + folder_name + '/' + i)
     all_data_for_a_label = []
     for j in instances_in_a_label:
-    # uncomment to debug
-    #   if counter < 2:
-        f = open("training_dataset/" + folder_name + '/' + i + '/' + j, "r", encoding='latin-1')
-        raw_data = f.read()
-        preprocessed_data = re.sub('[^a-zA-Z]', ' ', raw_data).lower()
-        preprocessed_data = preprocessed_data.split()
-        preprocessed_data = [word for word in preprocessed_data if word not in stopwords.words('english')]
-        preprocessed_data = ' '.join(preprocessed_data)
-        all_data.append([preprocessed_data, i])
-    #        counter += 1
-    # counter = 0
+        # uncomment to debug
+        if counter < 2:
+            f = open("training_dataset/" + folder_name + '/' + i + '/' + j, "r", encoding='latin-1')
+            raw_data = f.read()
+            preprocessed_data = re.sub('[^a-zA-Z]', ' ', raw_data).lower()
+            preprocessed_data = preprocessed_data.split()
+            preprocessed_data = [word for word in preprocessed_data if word not in stopwords.words('english')]
+            preprocessed_data = ' '.join(preprocessed_data)
+            all_data.append([preprocessed_data, i])
+            counter += 1
+    counter = 0
 
 all_data = np.asarray(all_data)
 df = pd.DataFrame(all_data)
